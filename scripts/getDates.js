68f40bf5f04fa002a8030343128fedb6a7a0913b
evansessoun <essoun.evans@gmail.com>
2023-09-30 13:27:46 +0000

    modified homepage

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,8 +1,21 @@
+const pageVisit = document.querySelector("#visits");
+
 let date = new Date();
 let year = date.getFullYear();
 let lastModification = document.lastModified;
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
 
 document.getElementById("year").innerHTML = `&copy; ${year}`;
 document.getElementById(
   "lastModified"
 ).innerHTML = `Last Modification: ${lastModification}`;
+
+if (numVisits !== 0) {
+  pageVisit.textContent = numVisits;
+} else {
+  pageVisit.textContent = "Your first visit, Welcome";
+}
+
+numVisits++;
+
+window.localStorage.setItem("numVisits-ls", numVisits);
